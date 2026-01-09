602f49f52c8ed809bf0a8e40b7ef804156875f41
Jong-Hoon Kim <kimfinale@gmail.com>
2026-01-09 14:36:17 +0900

    simulation.qmd rendered and files deleted in the R folder

--- R/region.r
+++
@@ -1,8 +0,0 @@
-# region.R
-# This file defines a region in which an outbreak may occur.
-# Each function returns a fixed set of region parameters.
-region <- function(id) {
-  pop <- 1e3 # population size
-  ok_prob <- runif(1, 0.01, 0.99) # outbreak probability
-  return(list(id = id, pop = pop, ok_prob = ok_prob))
-}
